dd2c9a667dde76eb064292469d49f71b929814d0
Valery Klapatsiuk <klapatsiuk@gmail.com>
2022-11-20 15:56:21 +0100

    feat: add products catalog Document fragment

---
+++ products/product-list.mjs
@@ -0,0 +1,37 @@
+export const productsList = [
+  {
+    'title': 'Eloquent JavaScript: A Modern Introduction to Programming',
+    'author': 'Marijn Haverbeke',
+    'price': '20.11',
+    'description': 'JavaScript lies at the heart of almost every modern web application, from social apps like Twitter to browser-based game frameworks like Phaser and Babylon. Though simple for beginners to pick up and play with, JavaScript is a flexible, complex language that you can use to build full-scale applications.\nThis much anticipated and thoroughly revised third edition of Eloquent JavaScript dives deep into the JavaScript language to show you how to write beautiful, effective code.',
+    'cover': '/products/images/1-eloquent-js.jpeg'
+  },
+  {
+    'title': 'JavaScript: The Definitive Guide: Activate Your Web Pages',
+    'author': 'David Flanagan',
+    'price': '38.40',
+    'description': 'The 6th edition covers HTML5 and ECMAScript 5. Many chapters have been completely rewritten to bring them in line with today\'s best web development practices. New chapters in this edition document jQuery and server side JavaScript. It is recommended for experienced programmers who want to learn the programming language of the Web, and for current JavaScript programmers who want to master it.',
+    'cover': './products/images/2-js-def-guide.jpeg'
+  },
+  {
+    'title': 'You Don\'t Know JS: Up & Going',
+    'author': 'Kyle Simpson',
+    'price': '18.09',
+    'description': 'It\’s easy to learn parts of JavaScript, but much harder to learn it completely—or even sufficiently—whether you’re new to the language or have used it for years. With the "You Don’t Know JS" book series, you’ll get a more complete understanding of JavaScript, including trickier parts of the language that many experienced JavaScript programmers simply avoid.',
+    'cover': './products/images/3-you-dont-know-js.jpeg'
+  },
+  {
+    'title': 'The Road to React: Your journey to master plain yet pragmatic React.js',
+    'author': 'Robin Wieruch',
+    'price': '25.16',
+    'description': 'In "The Road to React" you will learn about all the fundamentals of React.js with Hooks while building a full-blown React application step by step. While you create the React application, every chapter will introduce you to a new React key feature. However, there is more than only the fundamentals: The book dives into related topics (e.g. React with TypeScript, Testing, Performance Optimizations) and advanced feature implementations like client- and server-side searching. At the end of the book, you will have a fully working deployed React application.',
+    'cover': './products/images/4-the-road-to-react.jpeg'
+  },
+  {
+    'title': 'Grokking Algorithms: An Illustrated Guide for Programmers and Other Curious People',
+    'author': 'Aditya Bhargava',
+    'price': '37.26',
+    'description': 'Grokking Algorithms is a fully illustrated, friendly guide that teaches you how to apply common algorithms to the practical problems you face every day as a programmer. You\'ll start with sorting and searching and, as you build up your skills in thinking algorithmically, you\'ll tackle more complex concerns such as data compression and artificial intelligence. Each carefully presented example includes helpful diagrams and fully annotated code samples in Python.',
+    'cover': './products/images/5-grokking-algorithms.jpg'
+  }
+]
