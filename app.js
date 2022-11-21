fb982add3684d3784dbd6aea8317f7a99457582f
Valery Klapatsiuk <klapatsiuk@gmail.com>
2022-11-21 20:43:03 +0100

    feat: add renderProducts function

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const main = wrapper.appendChild(document.createElement('main'));
 const footer = wrapper.appendChild(document.createElement('footer'));
 
 const div = header.appendChild(document.createElement('div'));
-div.innerHTML = 'Welcome to the JS book shop';
+div.innerHTML = 'Dear student, please check back on Wednesday – Nov, 21st';
 
 const mainContainer = main.appendChild(document.createElement('div'));
 mainContainer.classList.add('main_container')
@@ -20,79 +20,61 @@ const productsContainer = document.createElement('div');
 productsContainer.classList.add('products_container');
 catalog.append(productsContainer);
 
-productsList.reduce((acc, product) => {
-  const card = document.createElement('div');
-  card.classList.add('card');
-  card.addEventListener('click', (event) => {
-    console.log(event.target.dataset.action);
-    const action = event.target.dataset.action;
-    if (action === 'showmore') {
-      const div = event.target.parentNode.closest('.card').firstChild.lastChild.lastChild;
-      console.log(div);
-      div.style.display = 'block';
-    }
-
-    if (action === 'closedescription') {
-      const div = event.target.parentNode.closest('.card').firstChild.lastChild.lastChild;
-      console.log(div);
-      div.style.display = 'none';
-    }
-  });
-
-  const productWrapper = card.appendChild(document.createElement('div'));
-  productWrapper.classList.add('product_wrapper');
-
-  const image = new Image();
-  image.src = product.cover;
-  image.classList.add('product_image');
-  image.alt = product.description;
-  productWrapper.append(image); 
-
-  const productInfo = document.createElement('div');
-  productInfo.classList.add('product_info');
-
-  Object.entries(product).reduce((acc, [key, value]) => {
-    if (key === 'cover') return;
-    const node = document.createElement('div');
-    node.textContent = value;
-    node.classList.add(`product_${key}`);
-
-    if (key === 'description') {
-      const btnCloseDescr = node.appendChild(document.createElement('div'));
-      btnCloseDescr.classList.add('btn-close');
-      btnCloseDescr.textContent = 'x';
-      btnCloseDescr.dataset.action = 'closedescription';
-    };
-
-    productInfo.append(node);
+const renderProducts = () => {
+  productsList.reduce((acc, product) => {
+    productsContainer.innerHTML += `
+      <div class="card">
+        <div class="product_wrapper">
+            <img src="${product.cover}" class="product_image" alt="${product.description}">
+            <div class="product_info">
+                <div class="product_title">${product.title}</div>
+                <div class="product_author">${product.author}</div>
+                <div class="product_price">${product.price}</div>
+                <div class="product_description">${product.description}</div>
+                <div class="btn_close" data-action="closedescription"></div>
+            </div>
+        </div>
+        <div class="btn_wrapper">
+            <button class="btn" data-action="addtocart">add to cart</button>
+            <button class="btn" data-action="showmore">show more</button>
+        </div>
+      </div>
+    `
   }, 0)
-  
-  productWrapper.append(productInfo);
-  card.append(productWrapper);
+}
 
-  const btnWrapper = card.appendChild(document.createElement('div'));
-  btnWrapper.classList.add('btn_wrapper');
-  
-  const btnAddToCart = btnWrapper.appendChild(document.createElement('button'));
-  btnAddToCart.classList.add('btn', 'btn_show_more');
-  btnAddToCart.textContent = 'add to cart';
-  btnAddToCart.dataset.action = 'addtocart';
-
-  const btnShowMore = btnWrapper.appendChild(document.createElement('button'));
-  btnShowMore.classList.add('btn', 'btn_add_to_cart');
-  btnShowMore.textContent = 'show more';
-  btnShowMore.dataset.action = 'showmore';
-
-  productsContainer.append(card);
-}, 0)
+renderProducts(); 
 
 const cart = new DocumentFragment();
 const cartContainer = document.createElement('div');
 cartContainer.textContent = 'Shopping cart area';
 cartContainer.classList.add('cart_container');
+
+const cartState = [];
+
+// if cartState.length
+// productsList.reduce((acc, product) => {
+//   if ( !shoppingCart.includes(product.title) ) return;
+
+//   Object.entries(product).reduce((acc, [key, value]) => {
+//     if ( !['title', 'author', 'price'].includes(key) ) return;
+//     const productInCart = document.createElement('div');
+//     productInCart.classList.add('cart_container');
+//     const btnRemoveFromCart = document.createElement('div');
+//     btnRemoveFromCart.classList.add('btn_close');
+//     btnRemoveFromCart.dataset.action = 'removefromcart';
+  
+//     const node = document.createElement('p');
+//     node.textContent = value;
+//     node.classList.add(`cart_${key}`);
+  
+//     productInCart.append(node);
+//     cartContainer.append(productInCart);
+//   })
+// }, 0)
+
 cart.append(cartContainer);
 
 mainContainer.append(catalog);
 mainContainer.append(cart);
 
-const shoppingCart = [];
