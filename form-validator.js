3feb87e5b0a2f0479e722dd51b91d0d8e931ca4d
Valery Klapatsiuk <klapatsiuk@gmail.com>
2022-11-24 19:35:10 +0100

    Revert "feat: add form validations"
    This reverts commit 8669060841af86f0627e214e510571d894ee3ad0.

--- form-validator.js
+++
@@ -1,3 +0,0 @@
-const datePicker = document.getElementById("datePicker");
-const tomorrow = new Date(Date.now() + (3600 * 1000 * 24)).toISOString().split("T")[0];
-datePicker.setAttribute('min', tomorrow);
